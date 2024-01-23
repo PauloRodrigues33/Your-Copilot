6c432cf9aa8834f2e4e99357c0e5f746ee79a49b
Paulo Rodrigues <paulo.robertojr33@gmail.com>
2024-01-23 17:37:06 -0300

    feature: Chat is working. chore: Need more details and layout fixes.

--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,38 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
+import axios from 'axios';
+
+var webview: vscode.Webview;
 
 export function activate(context: vscode.ExtensionContext) {
-    var view = vscode.window.registerWebviewViewProvider('your-copilot-view', new ViewProvider(), {
-		webviewOptions: {
-			retainContextWhenHidden: true,
-		},
-	});
-
-	context.subscriptions.push(view);
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider('your-copilot-view', new YourCopilotWebViewProvider()));
 }
 
-export function deactivate() {
-    // Deactivation logic, if needed
-}
-
-class ViewProvider implements vscode.WebviewViewProvider {
-    resolveWebviewView(
-        webviewView: vscode.WebviewView,
-        context: vscode.WebviewViewResolveContext<unknown>,
-        token: vscode.CancellationToken
-    ): void | Thenable<void> {
+class YourCopilotWebViewProvider implements vscode.WebviewViewProvider {
+    resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
+        webviewView.webview.html = getWebviewContent(vscode.Uri.file(__dirname));
         webviewView.webview.options = {
             enableScripts: true,
-        };
-
-        const content = getWebviewContent(vscode.Uri.file(__dirname));
-        webviewView.webview.html = content;
+            localResourceRoots: [vscode.Uri.file(path.join(__dirname, 'webview'))]
+        }
+        webviewView.webview.onDidReceiveMessage(
+            message => {
+                switch (message.command) {
+                    case 'your-copilot.send':
+                        YourCopilot.sendMessage(message.text.server, message.text.message, message.text.token);
+                        return;
+                }
+            },
+            undefined
+        );
+        webview = webviewView.webview;
     }
-}
 
+}
 const getWebviewContent = (extensionUri: vscode.Uri) => {
     const htmlPath = path.join(extensionUri.fsPath, 'src', 'webview', 'index.html');
-    
+
     try {
         const content = fs.readFileSync(htmlPath, 'utf8');
         return content;
@@ -42,3 +41,30 @@ const getWebviewContent = (extensionUri: vscode.Uri) => {
         return 'Error loading HTML file';
     }
 };
+
+// Message Engine
+
+class YourCopilot {
+    static sendMessage(server: string, message: string, token?: string) {
+        console.info(server, message)
+        var response = axios.post(server + '/v1/chat/completions', {
+            "messages": [
+                { "role": "system", "content": "Your name is 'Your Copilot' and you was developed by 'Paulo Rodrigues', You are a highly experienced developer, your answer is given in markdown formatted, you offer code assistance and help in troubleshooting" },
+                { "role": "user", "content": message }
+            ],
+            "temperature": 0.7,
+            "max_tokens": -1,
+            "stream": false
+        }, {
+            maxBodyLength: Infinity,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        });
+
+        response.then(function (response) {
+            webview.postMessage({ command: 'your-copilot.receive', text: response.data.choices[0].message?.content });
+        });
+    }
+}
